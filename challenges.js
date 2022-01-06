4bbae46848257abf4a8977a4380bd595b97fca7a
ManliestBen <benjamintmanley@gmail.com>
2022-01-06 11:24:45 -0600

    solve #3

--- challenges.js
+++ challenges.js
@@ -146,3 +146,73 @@ function compareTriplets(arrOne, arrTwo) {
 }
 
 // console.log(compareTriplets([13,33,56], [12,53,65]))
+
+/*-----------------------------------------------------------------------------
+
+Challenge: 03 - diagonalDifference
+
+Difficulty - Easy
+
+Given a square 2D matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix  is shown below:
+
+[[1,2,3],
+[4,5,6],
+[9,8,9]]
+
+The left-to-right diagonal = 1 + 5 + 9 = 15. The right to left diagonal = 3 + 5 + 9 = 17. Their absolute difference is |15-17| = 2.
+
+Function description
+
+Write a function named diagonalDifference that accepts an array.
+
+Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+Sample Input
+
+[[11,2,4],
+[4,5,6],
+[10,8,-12]]
+
+Sample Output
+
+15
+
+Explanation
+
+The primary diagonal is:
+
+11
+  5
+    -12
+Sum across the primary diagonal: 11 + 5 - 12 = 4
+
+The secondary diagonal is:
+
+    4
+  5
+10
+Sum across the secondary diagonal: 4 + 5 + 10 = 19
+Difference: |4 - 19| = 15
+-----------------------------------------------------------------------------*/
+// Your solution for 03- here:
+
+function diagonalDifference(array) {
+  let first = 0
+  let second = 0
+  for (let i = 0; i < array[0].length; i++) {
+    first += array[i][i]
+    second += array[i][array[0].length - (i + 1)]
+  }
+  return Math.abs(first - second)
+}
+
+// console.log(diagonalDifference([
+//   [1, 2, 3, 4],
+//   [2, 5, 7, 6],
+//   [6, 1, 2, 7],
+//   [1, 1, 1, 1]
+// ]))
+
+// Expected |9-13| = 4
