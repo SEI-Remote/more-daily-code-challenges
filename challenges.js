4aca588840af03c35e22f9206b0908c327cbca2e
teradaian <teradaian@gmail.com>
2023-06-05 07:18:01 -0700

    reword problem 2 to match spec

--- challenges.js
+++ challenges.js
@@ -7,8 +7,9 @@ Prompt:
 
 - Write a function called twoSum that takes an array of numbers and a target number
   and returns the indeces of the two numbers in the array that add up to the target. 
-- You may not use the same element twice 
 - Indeces must be returned in the same relative order they appear within the array
+- You may not use the same element twice, and if no two numbers add up to the 
+  target, return the string 'No Sum'
 
 Example 1:
 Input: nums = [2,7,11,15], target = 9
@@ -20,8 +21,8 @@ Input: nums = [3,2,4], target = 6
 Output: [1,2]
 
 Example 3:
-Input: nums = [3,3], target = 6
-Output: [0,1]
+Input: nums = [8,7], target = 16
+Output: 'No Sum'
 
 -----------------------------------------------------------------------------*/
 // Your solution for 01- here:
@@ -33,69 +34,34 @@ Challenge: 02 - compareTriplets
 
 Difficulty - Easy
 
-Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+Prompt:
 
-The rating for Alice's challenge is the triplet a = [a[0], a[1], a[2]], and the rating for Bob's challenge is the triplet b = [b[0], b[1], b[2]].
+Alice and Bob each created one problem for HackerRank. A reviewer rates the two
+challenges, awarding points on a scale from 1 to 100 for three categories:
+problem clarity, originality, and difficulty.
 
-The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+The rating for Alice's challenge is the triplet a = [a[0], a[1], a[2]], 
+and the rating for Bob's challenge is the triplet b = [b[0], b[1], b[2]].
+
+Your task is to find their comparison points by comparing a[0] with b[0], 
+a[1] with b[1], and a[2] with b[2].
 
 If a[i] > b[i], then Alice is awarded 1 point.
 If a[i] < b[i], then Bob is awarded 1 point.
 If a[i] = b[i], then neither person receives a point.
 Comparison points is the total points a person earned.
 
-Given a and b, determine their respective comparison points.
+- Write a function called compareTriplets that accepts two arrays and returns
+  an array of the respective comparison points of those two arrays.
+- The returned array should return in the order of [Alice's points, Bob's points]
 
-Example
+Examples:
 
-a = [1, 2, 3]
-b = [3, 2, 1]
-For elements 0, Bob is awarded a point because b[0] > a[0] .
-For the equal elements a[1] and b[1], no points are earned.
-Finally, for elements 2, a[2] > b[2] so Alice receives a point.
-The return array is [1, 1] with Alice's score first and Bob's second.
-
-
-Constraints
-1 ≤ a[i] ≤ 100
-1 ≤ b[i] ≤ 100
-
-Sample Input 0
-[5, 6, 7]
-[3, 6, 10]
-
-Sample Output 0
-[1, 1]
-
-Explanation 0
-In this example, let's compare each individual score:
-
-a[0] > b[0], so Alice receives a point.
-a[1] = b[1], so nobody receives a point.
-a[2] > b[2], so Bob receives a point.
-Alice's comparison score is 1, and Bob's comparison score is 1. Thus, we return the array [1,1].
-
-Sample Input 1
-
-[17, 28, 30]
-[99, 16, 8]
-
-Sample Output 1
-[2, 1]
-
-Explanation 1
-
-Comparing the 0th elements,  so Bob receives a point.
-Comparing the 1st and 2nd elements, 28 > 16 and 30 > 8 so Alice receives two points.
-The return array is [2,1].
+compareTriplets([1, 2, 3], [3, 2, 1]) //=> [1, 1]
+compareTriplets([5, 6, 7], [3, 6, 10]) //=> [1, 1]
+compareTriplets([17, 28, 30], [99, 16, 8]) //=> [2, 1]
 
 
-Sample input:
-[13,33,56]
-[12,53,65]
-
-Result: 
-[1,2]
 -----------------------------------------------------------------------------*/
 // Your solution for 02- here:
 
