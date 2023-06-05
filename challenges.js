104dc0239873cc00bab3e72e8c0b353ba9e78fd3
teradaian <teradaian@gmail.com>
2023-06-05 06:40:06 -0700

    reword prompt for problem 1

--- challenges.js
+++ challenges.js
@@ -3,17 +3,17 @@ Challenge: 01 - twoSum
 
 Difficulty - Easy
 
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.  Name the function twoSum.
+Prompt:
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-The indices must be returned in the same relative order that they appear within the array.
+- Write a function called twoSum that takes an array of numbers and a target number
+  and returns the indeces of the two numbers in the array that add up to the target. 
+- Assume each input has exactly one solution, and you may not use the same element twice
+- Indeces must be returned in the same relative order they appear within the array
 
 Example 1:
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Output: Because nums[0] + nums[1] == 9, we return [0, 1].
-
 
 Example 2:
 Input: nums = [3,2,4], target = 6
@@ -22,11 +22,6 @@ Output: [1,2]
 Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
-
-Constraints:
-
-2 <= nums.length 
-Only one valid answer exists.
 
 -----------------------------------------------------------------------------*/
 // Your solution for 01- here:
