db6e89306d02977af439e986c7e829f7ee6cd1c6
teradaian <teradaian@gmail.com>
2023-06-05 10:49:18 -0700

    rewrite prompt for kangaroo, leaving more verbose example

--- challenges.js
+++ challenges.js
@@ -225,45 +225,30 @@ Challenge: 09 - kangaroo
 
 Difficulty - Easy
 
-You are choreographing a circus show with various animals. For one act, you are given two kangaroos on a number line ready to jump in the positive direction (i.e, toward positive infinity).
+Prompt: 
+
+You are choreographing a circus show with various animals. For one act, you are given two kangaroos
+on a number line ready to jump in the positive direction (i.e, toward positive infinity).
 
 The first kangaroo starts at location x1 and moves at a rate of v1 meters per jump.
 The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump.
-You have to figure out a way to get both kangaroos at the same location at the same time as part of the show. If it is possible, return YES, otherwise return NO.
+You have to figure out a way to get both kangaroos at the same location at the same time 
+as part of the show. If it is possible, return YES, otherwise return NO.
 
-Example
-x1 = 2
-v1 = 1
-x2 = 1
-v2 = 2
+Examples:
 
-After one jump, they are both at x = 3, so the answer is YES.
-
-
-Sample Input 0
-0, 3, 4, 2
-
-Sample Output 0
-YES
-
-Explanation 0
-
+kangaroo(0,3,4,2) //=> 'YES'
 The two kangaroos jump through the following sequence of locations:
-
 k1 (0, 3, 6, 9, 12)
 k2 (4, 6, 8, 10, 12)
+It is clear that the kangaroos meet at the same location (number 12 on the number line) 
+after same number of jumps (4 jumps), so we print YES.
 
-It is clear that the kangaroos meet at the same location (number 12 on the number line) after same number of jumps (4 jumps), so we print YES.
-
-Sample Input 1
-0, 2, 5, 3
-
-Sample Output 1
-NO
-
-Explanation 1
-
-The second kangaroo has a starting location that is ahead (further along the number line) of the first kangaroo's starting location (i.e., x2 > x1). Because the second kangaroo moves at a faster rate (meaning v2 > v1) and is already ahead of the first kangaroo, the first kangaroo will never be able to catch up. Thus, we print NO.
+kangaroo(0,2,5,3) //=> 'NO
+The second kangaroo has a starting location that is ahead (further along the number line) 
+of the first kangaroo's starting location (i.e., x2 > x1). 
+Because the second kangaroo moves at a faster rate (meaning v2 > v1) and is already ahead 
+of the first kangaroo, the first kangaroo will never be able to catch up. Thus, we print NO.
 
 -----------------------------------------------------------------------------*/
 // Your solution for 09- here:
