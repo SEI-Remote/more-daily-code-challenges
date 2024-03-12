ca979814b7e6aa557175a46a7f889eb07714e366
Ben Manley <benjamintmanley@gmail.com>
2024-03-12 08:55:46 -0500

    correct spelling error

--- challenges.js
+++ challenges.js
@@ -7,7 +7,7 @@ Prompt:
 
 - Write a function called twoSum that takes an array of numbers and a target number
   and returns the indeces of the two numbers in the array that add up to the target. 
-- Indeces must be returned in the same relative order they appear within the array
+- Indices must be returned in the same relative order they appear within the array
 - You may not use the same element twice, and if no two numbers add up to the 
   target, return the string 'No Sum'
 
